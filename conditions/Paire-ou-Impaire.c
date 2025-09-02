16f59595881ba7ad2a6cd264e64c308c32ac409e
Abdelilah-ISSWA <tomhary0011@gmail.com>
2025-09-02 17:05:33 +0100

    version2

---
+++ conditions/Paire-ou-Impaire.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int number;
+    printf("entrez un nombre:");
+    scanf("%d",&number);
+    if(number % 2 == 0 ){
+        printf("number pair\n");
+
+
+    }else{
+        printf("number impair\n");
+}
+}
